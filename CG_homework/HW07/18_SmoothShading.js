77181f5f74bd0f9a82102d62f37e0b0a79d99082
seungho <gomi090447@gmail.com>
2025-05-04 04:31:46 +0900

    fix: HW08

--- CG_homework/HW07/18_SmoothShading.js
+++ CG_homework/HW07/18_SmoothShading.js
@@ -21,25 +21,31 @@ const canvas = document.getElementById('glCanvas');
 const gl = canvas.getContext('webgl2');
 let shader;
 let lampShader;
-let textOverlay; 
+let phongShader;
+let gouraudShader;
+let textOverlay;
 let textOverlay2;
 let textOverlay3;
 let textOverlay4;
 let textOverlay5;
 let textOverlay6;
+let textOverlay7;
+let textOverlay8;
 let isInitialized = false;
 
 let viewMatrix = mat4.create();
 let projMatrix = mat4.create();
 let modelMatrix = mat4.create();
 let lampModelMatrix = mat4.create();
-let arcBallMode = 'CAMERA';     // 'CAMERA' or 'MODEL'
-let shadingMode = 'FLAT';       // 'FLAT' or 'SMOOTH'
-let renderingMode = 'PHONG '; // 'PHONG' or 'GOURAUD'
+let arcBallMode = "CAMERA"; // 'CAMERA' or 'MODEL'
+let shadingMode = "FLAT"; // 'FLAT' or 'SMOOTH'
+let renderingMode = "PHONG"; // 'PHONG' or 'GOURAUD'
+
+let shadingModeText = shadingMode + " (" + renderingMode + ")";
 
 const cylinder = new Cone(gl, 32);
 const lamp = new Cube(gl);
-const axes = new Axes(gl, 1.5); // create an Axes object with the length of axis 1.5
+// const axes = new Axes(gl, 1.5); // create an Axes object with the length of axis 1.5
 
 const cameraPos = vec3.fromValues(0, 0, 3);
 const lightPos = vec3.fromValues(1.0, 0.7, 1.0);
@@ -49,60 +55,79 @@ const lightSize = vec3.fromValues(0.1, 0.1, 0.1);
 // default of rotation sensitivity = 1.5, default of zoom sensitivity = 0.001
 const arcball = new Arcball(canvas, 5.0, { rotation: 2.0, zoom: 0.0005 });
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     if (isInitialized) {
         console.log("Already initialized");
         return;
     }
 
-    main().then(success => {
-        if (!success) {
-            console.log('program terminated');
-            return;
-        }
-        isInitialized = true;
-    }).catch(error => {
-        console.error('program terminated with error:', error);
-    });
+    main()
+        .then((success) => {
+            if (!success) {
+                console.log("program terminated");
+                return;
+            }
+            isInitialized = true;
+        })
+        .catch((error) => {
+            console.error("program terminated with error:", error);
+        });
 });
 
+function updateShader() {
+    shader = renderingMode === "PHONG" ? phongShader : gouraudShader;
+    shader.use();
+    shader.setMat4("u_projection", projMatrix);
+    shader.setVec3("material.diffuse", vec3.fromValues(1.0, 0.5, 0.31));
+    shader.setVec3("material.specular", vec3.fromValues(0.5, 0.5, 0.5));
+    shader.setFloat("material.shininess", 16);
+    shader.setVec3("light.position", lightPos);
+    shader.setVec3("light.ambient", vec3.fromValues(0.2, 0.2, 0.2));
+    shader.setVec3("light.diffuse", vec3.fromValues(0.7, 0.7, 0.7));
+    shader.setVec3("light.specular", vec3.fromValues(1.0, 1.0, 1.0));
+    shader.setVec3("u_viewPos", cameraPos);
+}
+
 function setupKeyboardEvents() {
-    document.addEventListener('keydown', (event) => {
-        if (event.key == 'a') {
-            if (arcBallMode == 'CAMERA') {
-                arcBallMode = 'MODEL';
-            }
-            else {
-                arcBallMode = 'CAMERA';
+    document.addEventListener("keydown", (event) => {
+        if (event.key == "a") {
+            if (arcBallMode == "CAMERA") {
+                arcBallMode = "MODEL";
+            } else {
+                arcBallMode = "CAMERA";
             }
             updateText(textOverlay, "arcball mode: " + arcBallMode);
-        }
-        else if (event.key == 'r') {
+        } else if (event.key == "r") {
             arcball.reset();
-            modelMatrix = mat4.create(); 
-            arcBallMode = 'CAMERA';
+            modelMatrix = mat4.create();
+            arcBallMode = "CAMERA";
             updateText(textOverlay, "arcball mode: " + arcBallMode);
-        }
-        else if (event.key == 's') {
+        } else if (event.key == "s") {
             cylinder.copyVertexNormalsToNormals();
             cylinder.updateNormals();
-            shadingMode = 'SMOOTH';
-            updateText(textOverlay2, "shading mode: " + shadingMode);
+            shadingMode = "SMOOTH";
             render();
-        }
-        else if (event.key == 'f') {
+        } else if (event.key == "f") {
             cylinder.copyFaceNormalsToNormals();
             cylinder.updateNormals();
-            shadingMode = 'FLAT';
-            updateText(textOverlay2, "shading mode: " + shadingMode);
+            shadingMode = "FLAT";
             render();
+        } else if (event.key == "g") {
+            renderingMode = "GOURAUD";
+        } else if (event.key == "p") {
+            renderingMode = "PHONG";
         }
+        updateText(
+            textOverlay2,
+            "shading mode: " + shadingMode + " (" + renderingMode + ")"
+        );
+        updateShader();
     });
 }
 
 function initWebGL() {
     if (!gl) {
-        console.error('WebGL 2 is not supported by your browser.');
+        console.error("WebGL 2 is not supported by your browser.");
         return false;
     }
 
@@ -110,51 +135,61 @@ function initWebGL() {
     canvas.height = 700;
     resizeAspectRatio(gl, canvas);
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0.7, 0.8, 0.9, 1.0);
-    
+    gl.clearColor(0.1, 0.1, 0.1, 1.0);
+
     return true;
 }
 
 async function initShader() {
-    const vertexShaderSource = await readShaderFile('shVert.glsl');
-    const fragmentShaderSource = await readShaderFile('shFrag.glsl');
+    const vertexShaderSource = await readShaderFile("shVert.glsl");
+    const fragmentShaderSource = await readShaderFile("shFrag.glsl");
     return new Shader(gl, vertexShaderSource, fragmentShaderSource);
 }
 
 async function initLampShader() {
-    const vertexShaderSource = await readShaderFile('shLampVert.glsl');
-    const fragmentShaderSource = await readShaderFile('shLampFrag.glsl');
+    const vertexShaderSource = await readShaderFile("shLampVert.glsl");
+    const fragmentShaderSource = await readShaderFile("shLampFrag.glsl");
     return new Shader(gl, vertexShaderSource, fragmentShaderSource);
+}
+
+async function initShaders() {
+    const vsPhongSrc = await readShaderFile("phongVert.glsl");
+    const fsPhongSrc = await readShaderFile("phongFrag.glsl");
+    const vsGouraudSrc = await readShaderFile("gouraudVert.glsl");
+    const fsGouraudSrc = await readShaderFile("gouraudFrag.glsl");
+
+    phongShader = new Shader(gl, vsPhongSrc, fsPhongSrc);
+    gouraudShader = new Shader(gl, vsGouraudSrc, fsGouraudSrc);
 }
 
 function render() {
     // clear canvas
-    gl.clearColor(0.1, 0.1, 0.1, 1.0);
+    // gl.clearColor(0.1, 0.1, 0.1, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.enable(gl.DEPTH_TEST);
 
-    if (arcBallMode == 'CAMERA') {
+    if (arcBallMode == "CAMERA") {
         viewMatrix = arcball.getViewMatrix();
-    }
-    else { // arcBallMode == 'MODEL'
+    } else {
+        // arcBallMode == 'MODEL'
         modelMatrix = arcball.getModelRotMatrix();
         viewMatrix = arcball.getViewCamDistanceMatrix();
     }
 
     // drawing the cylinder
-    shader.use();  // using the cylinder's shader
-    shader.setMat4('u_model', modelMatrix);
-    shader.setMat4('u_view', viewMatrix);
-    shader.setVec3('u_viewPos', cameraPos);
+    shader.use(); // using the cylinder's shader
+    shader.setMat4("u_model", modelMatrix);
+    shader.setMat4("u_view", viewMatrix);
+    shader.setVec3("u_viewPos", cameraPos);
     cylinder.draw(shader);
 
     // drawing the lamp
     lampShader.use();
-    lampShader.setMat4('u_view', viewMatrix);
+    lampShader.setMat4("u_view", viewMatrix);
     lamp.draw(lampShader);
 
     // drawing the axes (using the axes's shader: see util.js)
-    axes.draw(viewMatrix, projMatrix);
+    // axes.draw(viewMatrix, projMatrix);
 
     // call the render function the next time for animation
     requestAnimationFrame(render);
@@ -163,24 +198,26 @@ function render() {
 async function main() {
     try {
         if (!initWebGL()) {
-            throw new Error('WebGL initialization failed');
+            throw new Error("WebGL initialization failed");
         }
-        
+
         // View transformation matrix (camera at cameraPos, invariant in the program)
         mat4.translate(viewMatrix, viewMatrix, cameraPos);
 
         // Projection transformation matrix (invariant in the program)
         mat4.perspective(
             projMatrix,
-            glMatrix.toRadian(60),  // field of view (fov, degree)
+            glMatrix.toRadian(60), // field of view (fov, degree)
             canvas.width / canvas.height, // aspect ratio
             0.1, // near
             100.0 // far
         );
 
         // creating shaders
-        shader = await initShader();
+        await initShaders();
+        shader = phongShader;
         lampShader = await initLampShader();
+        updateShader();
 
         shader.use();
         shader.setMat4("u_projection", projMatrix);
@@ -200,24 +237,41 @@ async function main() {
         const lampModelMatrix = mat4.create();
         mat4.translate(lampModelMatrix, lampModelMatrix, lightPos);
         mat4.scale(lampModelMatrix, lampModelMatrix, lightSize);
-        lampShader.setMat4('u_model', lampModelMatrix);
+        lampShader.setMat4("u_model", lampModelMatrix);
 
         textOverlay = setupText(canvas, "arcball mode: " + arcBallMode);
-        textOverlay2 = setupText(canvas, "shading mode: " + shadingMode, 2);
+        textOverlay2 = setupText(canvas, "shading mode: " + shadingModeText, 2);
         textOverlay3 = setupText(canvas, "press 'a' to change arcball mode", 3);
         textOverlay4 = setupText(canvas, "press 'r' to reset arcball", 4);
-        textOverlay5 = setupText(canvas, "press 's' to switch to smooth shading", 5);
-        textOverlay6 = setupText(canvas, "press 'f' to switch to flat shading", 6);
+        textOverlay5 = setupText(
+            canvas,
+            "press 's' to switch to smooth shading",
+            5
+        );
+        textOverlay6 = setupText(
+            canvas,
+            "press 'f' to switch to flat shading",
+            6
+        );
+        textOverlay7 = setupText(
+            canvas,
+            "press 'g' to switch to Gouraud shading",
+            7
+        );
+        textOverlay8 = setupText(
+            canvas,
+            "press 'p' to switch to Phong shading",
+            8
+        );
         setupKeyboardEvents();
 
         // call the render function the first time for animation
         requestAnimationFrame(render);
 
         return true;
-
     } catch (error) {
-        console.error('Failed to initialize program:', error);
-        alert('Failed to initialize program');
+        console.error("Failed to initialize program:", error);
+        alert("Failed to initialize program");
         return false;
     }
 }
